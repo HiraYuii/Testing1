b3d44d22274da017c05d212515daf9a5744a5bad
HiraYuii <phamngocduy96@gmail.com>
2018-03-19 16:09:19 +0700

    push

---
+++ app/src/main/java/com/example/duy/myapplication/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.duy.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Duy on 19/03/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
